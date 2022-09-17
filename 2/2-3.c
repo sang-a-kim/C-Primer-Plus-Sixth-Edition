302b5da9ba1690cfb5d4a63894f99d514e53d837
김상아 Sang-A Kim <blossom78325@gmail.com>
2022-09-17 23:09:08 +0900

    2-3. control character

---
+++ 2/2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Be happy\n");
+  printf("12312312313123\n");
+  printf("My\tfriend\n");
+  printf("Goot\bd\tchance\n");
+  printf("Cow\rW\a\n");
+
+  return 0;
+}
