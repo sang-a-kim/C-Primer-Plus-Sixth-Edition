cc27bb2025a083537725b55f7b22dfa7ead5f685
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-10 09:18:47 +0900

    4-4: strlen function

---
+++ 4/4-4/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+ // char str5 = (char*)malloc(sizeof(char) * 100);
+  // str5[0] = 'H'; str5[1] = 'e'; str5[2] = 'l' str5[3] = 'l', str5[4] = 'o'; str5[5] = '\0';
+  // printf("%zu %zu\n", sizeof(str5), strlen(str5));
