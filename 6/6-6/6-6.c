f0c5be481832b7554a4d3f11d84cf108341304d3
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-21 11:34:44 +0900

    6-6: _Bool type

---
+++ 6/6-6/6-6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+  int i;
+  // i = 5;
+  // i == 5;
+  // 5 = i;
+  // 5 == i;
+
+  // while (i = 5)
+  // {
+  //   printf("Hi\n");
+  // };
+
+  // while(i == 5)
+  // {
+  //   printf("Hey\n");
+  // };
+
+  // _Bool boolean_true = (2 > 1);
+  // _Bool boolean_false = (2 < 1);
+
+  // printf("True is %d\n", boolean_true);
+  // printf("False is %d\n", boolean_false);
+
+  // printf(boolean_true ? "true" : 'false');
+  // printf("\n");
+  // printf(boolean_false ? "true" : "false");
+
+  bool bt = true;
+  bool bf = false;
+
+  printf("\n");
+  printf("True is %d\n", bt);
+  printf("False is %d\n", bf);
+
+  return 0;
+}
