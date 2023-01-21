15d9c6aa15266cc9574bd07b63e167666cc260db
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-21 11:10:52 +0900

    6-5: true and false

---
+++ 6/6-5/6-5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  // int tv, fv;
+  // tv = (1 < 2);
+  // fv = (1 > 2);
+
+  // printf("True is %d\n", tv);
+  // printf("False is %d\n", fv);
+
+  // int i = 5;
+  // while (i)
+  //   printf("%d is true\n", i--);
+  // printf("%d is false\n", i);
+
+  while(1) // infinite loop
+  {
+
+  }
+
+  return 0;
+}
