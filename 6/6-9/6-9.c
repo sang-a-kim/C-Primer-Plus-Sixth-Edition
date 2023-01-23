1c83038e8cdbc6ff6842380e0e8a64bc505ada16
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-23 09:32:13 +0900

    6-9: for loop is flexable

---
+++ 6/6-9/6-9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  // for ( ; ;)
+  // {
+  //   printf("I love you");
+  // }
+
+  int i = 0;
+  for(printf("Let's go!\n"); i != 7; scanf("%d", &i));
+
+  return 0;
+}
