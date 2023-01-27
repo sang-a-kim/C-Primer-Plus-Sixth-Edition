6b38dcdb8f846ea3bdf62afed48acaabfe28aa17
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-27 21:51:37 +0900

    7-13: how not to use go to statement

---
+++ 7/7-13/7-13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+  /* if else */
+  int size = 15, cost;
+
+  if (size < 10)
+    goto a;
+  goto b;
+
+a: cost = 50 * size;
+b: cost = 100 * size;
+
+  if (size < 10)
+    cost = 50 * size;
+  cost = 100 * size;
+
+  /* loop */
+  char c;
+
+read: c = getchar();
+  putchar(c);
+  if (c == '.') goto quit;
+  goto read;
+quit:
+
+  while(1)
+  {
+    c = getchar();
+    putchar(c);
+    if (c == '.') break;
+  }
+
+  return 0;
+}
