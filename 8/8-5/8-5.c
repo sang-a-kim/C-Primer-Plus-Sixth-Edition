a91e6ae47c01bccf186a31e7c042005018a43913
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-01-31 08:21:26 +0900

    8-5: get input of both character and number

---
+++ 8/8-5/8-5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void display(char cr, int lines, int width);
+
+int main()
+{
+  char c;
+  int rows, cols;
+
+  // while (1)
+  // {
+  //   scanf("%c %d %d", &c, &rows, &cols);
+  //   while (getchar() != '\n')
+  //     continue;   
+    
+  //   display(c, rows, cols);
+  //   if (c == '\n')
+  //     break; 
+  // }
+
+  printf("Input one character and two integers\n");
+  while ((c = getchar()) != '\n')
+  {
+    scanf("%d %d", &rows, &cols);
+    while (getchar() != '\n')
+      continue;
+    display(c, rows, cols);
+    printf("Input another character and two integers:\n");
+    printf("Print Enter to quit.\n");
+  }
+
+  return 0;
+}
+
+void display(char cr, int lines, int width)
+{
+  int row, col;
+
+  for (row = 1; row <= lines; row++)
+  {
+    for (col = 1; col <= width; col++)
+      putchar(cr);
+    putchar('\n');
+  }
+}
