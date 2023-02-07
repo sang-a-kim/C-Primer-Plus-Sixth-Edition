2db3e5267c65fc0c4f404bf17b173e0c0d08eabf
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-02-08 08:08:40 +0900

    9-6: recursion

---
+++ 9/9-6/9-6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void my_func(int);
+
+int main()
+{
+  my_func(1);
+
+  return 0;
+}
+
+void my_func(int n)
+{
+  printf("Level %d, address of variable n = %p\n", n, &n);
+
+  if (n <=4)
+    my_func(n + 1);
+ 
+  printf("Level %d, address of variable n = %p\n", n, &n);
+}
